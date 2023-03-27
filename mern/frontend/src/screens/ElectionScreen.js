61672cc65a1186030d74882e7f63ffca43cf052a
badguy-00@github.com <balajisankar555@gmail.com>
2023-03-27 23:41:02 +0530

    Election Screen

--- mern/frontend/src/screens/ElectionScreen.js
+++ mern/frontend/src/screens/ElectionScreen.js
@@ -12,6 +12,9 @@ const ElectionScreen = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo: user } = userLogin;
+
   const electionDetails = useSelector((state) => state.electionDetails);
   const { loading, error, election } = electionDetails;
 
@@ -22,6 +25,12 @@ const ElectionScreen = () => {
   const resultHandler = () => {
     navigate(`/result/${id}`);
   };
+
+  const finishHandler = () => {};
+
+  const startHandler = () => {};
+
+  const voteHandler = () => {};
 
   return (
     <div>
@@ -35,29 +44,157 @@ const ElectionScreen = () => {
         <Message variant="danger">{error}</Message>
       ) : (
         <>
+          <h1>Election {election._id}</h1>
           <Row>
-            <Col md={3}>
+            <Col md={8}>
               <ListGroup variant="flush">
                 <ListGroup.Item>
                   <h3>{election.name}</h3>
+                  <p>
+                    <strong>Description: </strong>
+                    {election.description}
+                  </p>
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  Description: {election.description}
+                  <h3>Election Details</h3>
+                  {election.isStarted ? (
+                    <Message variant="success">
+                      Started on {election.startedAt}
+                    </Message>
+                  ) : (
+                    <Message variant="danger">Not Started</Message>
+                  )}
+                  {election.isStarted && election.isFinished ? (
+                    <Message variant="success">
+                      Finished on {election.finishedAt}
+                    </Message>
+                  ) : (
+                    <Message variant="warning">Running Now</Message>
+                  )}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <h3>Candidate Details</h3>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col md={2}>
+                        <h5>
+                          <strong>Name</strong>
+                        </h5>
+                      </Col>
+                      <Col md={2}>
+                        <h5>
+                          <strong>Gender</strong>
+                        </h5>
+                      </Col>
+                      <Col md={3}>
+                        <h5>
+                          <strong>Description</strong>
+                        </h5>
+                      </Col>
+                      <Col md={5}>
+                        <h5>
+                          <strong>View Profile</strong>
+                        </h5>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  {election.candidates?.map((candidate) => (
+                    <ListGroup.Item key={candidate._id}>
+                      <Row>
+                        <Col md={2}>
+                          <strong>{candidate.name}</strong>
+                        </Col>
+                        <Col md={2}>
+                          <strong>{candidate.gender}</strong>
+                        </Col>
+                        <Col md={3}>
+                          <strong>{candidate.description}</strong>
+                        </Col>
+                        <Col md={5}>
+                          <Button
+                            variant="primary"
+                            onClick={() => navigate(`/users/${candidate._id}`)}
+                          >
+                            View Candidate
+                          </Button>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
                 </ListGroup.Item>
               </ListGroup>
             </Col>
-            <Col md={3}>
-              <ListGroup.Item className="mx-auto">
-                <Col>
-                  <Button
-                    onClick={resultHandler}
-                    className="btn-block"
-                    type="button"
-                  >
-                    View Result
-                  </Button>
-                </Col>
-              </ListGroup.Item>
+            <Col md={4}>
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <h2>Election Summary</h2>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Eligible Voters</Col>
+                      <Col>{election.voters?.length}</Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Votes Recieved</Col>
+                      <Col>{election.voters?.length}</Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    {user.isAdmin ? (
+                      !election.isStarted ? (
+                        <Button
+                          onClick={startHandler}
+                          className="btn btn-block"
+                          type="button"
+                        >
+                          Start Election
+                        </Button>
+                      ) : election.isFinished ? (
+                        <Button
+                          onClick={resultHandler}
+                          className="btn btn-block"
+                          type="button"
+                        >
+                          View Result
+                        </Button>
+                      ) : (
+                        <Button
+                          onClick={finishHandler}
+                          className="btn btn-block"
+                          type="button"
+                        >
+                          Finish Election
+                        </Button>
+                      )
+                    ) : election.isStarted ? (
+                      election.isFinished ? (
+                        <Button
+                          onClick={resultHandler}
+                          className="btn btn-block"
+                          type="button"
+                        >
+                          View Result
+                        </Button>
+                      ) : (
+                        <Button
+                          onClick={voteHandler}
+                          className="btn btn-block"
+                          type="button"
+                        >
+                          Vote Now
+                        </Button>
+                      )
+                    ) : (
+                      <Button className="btn btn-block" type="button" disabled>
+                        Vote Now
+                      </Button>
+                    )}
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
             </Col>
           </Row>
         </>
