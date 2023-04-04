3069f1d9386afe79df8c3c86573c16a665760eff
badguy-00@github.com <balajisankar555@gmail.com>
2023-04-04 11:34:36 +0530

    Edit Election

--- frontend/src/screens/ElectionEditScreen.js
+++ frontend/src/screens/ElectionEditScreen.js
@@ -7,16 +7,97 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import FormContainer from "../components/FormContainer";
 import Meta from "../components/Meta";
-// import { listProductDetails, updateProduct } from "../actions/productActions";
-// import { ELECTION_UPDATE_RESET } from "../constants/productConstants";
+import {
+  listElectionDetails,
+  updateElection,
+} from "../actions/electionActions";
+import { ELECTION_UPDATE_RESET } from "../constants/electionConstants";
 
 const ElectionEditScreen = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [isStarted, setIsStarted] = useState(true);
-  const [isFinished, setIsFinished] = useState(false);
 
-  return <div></div>;
+  const dispatch = useDispatch();
+  const history = useNavigate();
+  const { id } = useParams();
+
+  const electionDetails = useSelector((state) => state.electionDetails);
+  const { loading, error, election } = electionDetails;
+
+  const electionUpdate = useSelector((state) => state.electionUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = electionUpdate;
+
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: ELECTION_UPDATE_RESET });
+      history("/admin/electionlist");
+    } else {
+      if (!election.name || election._id !== id) {
+        dispatch(listElectionDetails(id));
+      } else {
+        setName(election.name);
+        setDescription(election.description);
+      }
+    }
+  }, [dispatch, history, id, election, successUpdate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      updateElection({
+        _id: id,
+        name,
+        description,
+      })
+    );
+  };
+
+  return (
+    <div>
+      <Meta title="Edit Election | E-Voting" />
+      <Link to="/admin/electionlist" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit Election</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="description">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Button type="submit" variant="primary" className="my-3">
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </div>
+  );
 };
 
 export default ElectionEditScreen;
