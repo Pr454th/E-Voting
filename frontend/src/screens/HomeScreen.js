513cc6af65792f150587de79697373e301ce4378
badguy-00@github.com <balajisankar555@gmail.com>
2023-05-09 10:12:46 +0530

    Home Page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getContract } from "../actions/contractActions";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -9,7 +10,56 @@ const HomeScreen = () => {
     dispatch(getContract());
   }, [dispatch]);
 
-  return <h3 className="text-center">HomePage</h3>;
+  return (
+    <div>
+      <Container className="p-0">
+        <Row className="text-white py-5">
+          <Col
+            className="text-center"
+            style={{
+              backgroundImage: 'url("bg-voting.jpg")',
+              backgroundSize: "stretch",
+              backgroundPosition: "center",
+            }}
+          >
+            <h1 className="text-white">Welcome to the Voting Website</h1>
+            <p className="lead">
+              Vote for your favorite candidates and make your voice heard!
+            </p>
+          </Col>
+        </Row>
+        <Row className="py-5">
+          <Col>
+            <h2>What is the Voting Website?</h2>
+            <p className="lead">
+              The Voting Website is an online platform where you can vote for
+              your favorite candidates in various elections and surveys.
+            </p>
+            <p className="lead">
+              We believe that everyone should have a say in the decisions that
+              affect their lives, and the Voting Website is our way of
+              empowering people to make their voices heard.
+            </p>
+          </Col>
+        </Row>
+        <Row className="bg-light py-5">
+          <Col>
+            <h2>Why Choose the Voting Website?</h2>
+            <p className="lead">
+              There are many reasons to choose the Voting Website:
+            </p>
+            <ul className="lead">
+              <li>Easy to use</li>
+              <li>Secure and reliable</li>
+              <li>Transparent and fair</li>
+              <li>Accessible to all</li>
+              <li>And much more!</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default HomeScreen;
