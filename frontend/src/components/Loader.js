2a8bc5acf47c048c88bee8055cc705002427600b
badguy-00@github.com <balajisankar555@gmail.com>
2023-05-27 13:36:52 +0530

    Loader Component size

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { Spinner } from "react-bootstrap";
 
-const Loader = () => {
+const Loader = (props) => {
   return (
     <Spinner
       animation="border"
       role="status"
       style={{
-        width: "100px",
-        height: "100px",
-        margin: "auto",
+        width: props.width,
+        height: props.width,
+        margin: props.margin,
         display: "block",
       }}
     >
       <span className="sr-only">Loading....</span>
     </Spinner>
   );
+};
+
+Loader.defaultProps = {
+  width: "100px",
+  height: "100px",
+  margin: "auto",
 };
 
 export default Loader;
